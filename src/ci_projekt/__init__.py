1e9e6cf923782109de4fe376cdfe587591bd08e4
nabil0n <isak.and@gmail.com>
2024-10-27 21:16:42 +0100

    init commit

---
+++ src/ci_projekt/__init__.py
@@ -0,0 +1,5 @@
+def main() -> None:
+    print("Hello from ci-projekt!")
+
+def add() -> int:
+    return 42
---
+++ tests/ci_projekt/test_init.py
@@ -0,0 +1,4 @@
+from ci_projekt import add
+
+def test_add() -> None:
+    assert 42 == add()
