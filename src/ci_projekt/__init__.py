32500dabee5b578b4e303bee132d63ce2debbeba
nabil0n <isak.and@gmail.com>
2024-11-02 22:16:17 +0100

    mid sync

--- src/ci_projekt/__init__.py
+++ src/ci_projekt/__init__.py
@@ -1,2 +1,10 @@
-def generate_list() -> int:
-    return 42
+def hello() -> str:
+    return "Hello from ci_projekt!"
+
+
+def add(a: int, b: int) -> int:
+    return a + b
+
+
+def multiply(a: float, b: int) -> float:
+    return a * b
--- tests/ci_projekt/test_init.py
+++
@@ -1,5 +0,0 @@
-from src.ci_projekt import generate_list
-
-
-def test_generate_list():
-    assert 42 == generate_list()
